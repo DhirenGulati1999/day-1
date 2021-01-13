7f4ab44fa2c7ff6285b7321b18188d0b516e8668
Dhirengulati1999 <dgulati@ex2india.com>
2021-01-13 16:34:32 +0530

    improved

--- 1.js
+++ 1.js
@@ -1,65 +1,89 @@
-var values = [10,1,9,2,8,3,7,4,6];
-function up(v)
+ var v=[];
+ var final=[];
+ function Entry()
+{
+    var p = document.getElementById("array").value;
+     final = p.split(",");
+    for(var i=0;i<final.length;i++)
+    {
+        v.push(parseFloat(final[i]));
+    }
+    console.log(v);
+}
+function up_helper()
+{
+    var a = up();
+    console.log(a);
+}
+function up()
 {
     return v.sort((a,b)=>a-b);
 }
-function down(v)
+function down_helper()
+{
+    var a = down();
+    console.log(a);
+}
+function down()
 {
     
     return v.sort((a,b)=>b-a);
 }
-function max(v)
+function max()
 {
     var temp = v[0];
-    for(var i=0;i<9;i++)
+    for(var i=0;i<v.length;i++)
     {
         temp = Math.max(v[i],temp);
     }
-    return temp;
+    console.log(temp);
 }
-function min(v)
+function min()
 {
-    var temp = values[0];
-    for(var i=0;i<9;i++)
+    var temp = v[0];
+    for(var i=0;i<v.length;i++)
     {
-        temp = Math.min(values[i],temp);
+        temp = Math.min(v[i],temp);
     }
-    return temp;
+    console.log(temp);
 }
-function sum(v)
+function sum_helper()
+{
+    var a = sum();
+    console.log(a);
+}
+function sum()
 {
     var temp = 0;
-    for(var i=0;i<9;i++)
+    for(var i=0;i<v.length;i++)
     {
         temp = temp + v[i];
     }
     return temp;
 }
-function median(v)
+function median()
 {
     if(v.length%2 != 0)
-        return v[Math.floor(v.length/2)];
-    return v[Math.floor(v.length/2)] + v[Math.floor((v.length-1)/2)];
+        console.log(v[Math.floor(v.length/2)]);
+    console.log((v[Math.floor(v.length/2)] + v[Math.floor((v.length-1)/2)])/2);
 }
-function mean(v)
+function mean()
 {
-    return sum(v)/v.length;
+    return (sum()/v.length);
 }
-function stdev(v)
+function mean_helper()
+{
+    var a = mean();
+    console.log(a);
+}
+function stdev()
 {
     var temp = 0;
-    var t = mean(v);
+    var t = mean();
     for(var i=0;i<v.length;i++)
     {
         temp = temp + (v[i]-t)*(v[i]-t);
     }
-    return Math.sqrt(temp/v.length);
+    console.log( Math.sqrt(temp/v.length));
 }
-console.log(max(values));
-console.log(min(values));
-console.log(up(values));
-console.log(sum(values));
-console.log(median(values));
-console.log(mean(values));
-console.log(stdev(values));
-console.log(down(values));
+
