24a44bc003351ae45bd5bcd66a876404ed8649ee
Dhirengulati1999 <dgulati@ex2india.com>
2021-01-14 09:48:48 +0530

    updated version

--- 1.js
+++ 1.js
@@ -1,5 +1,22 @@
  var v=[];
  var final=[];
+ var up_1 = -1;
+ var down_1 = -1;
+ var up_store = [];
+var sum_1 = -1;
+var sum_sore = 0;
+var mean_1 = -1;
+var mean_store = 0;
+var min_1 = -1;
+var min_store = 0;
+var max_1 = -1;
+var max_store = 0;
+var st_1 = -1;
+var st_store = 0;
+var med_1 = -1;
+var med_store = 0;
+var down_1 = -1;
+var down_store = [];
  function Entry()
 {
     var p = document.getElementById("array").value;
@@ -12,8 +29,15 @@
 }
 function up_helper()
 {
-    var a = up();
-    console.log(a);
+    if(up_1 == -1)
+    {
+        var a = up();
+        console.log(a);
+        up_store = a;
+        up_1 = 0;
+        return;
+    }
+    console.log(up_store);
 }
 function up()
 {
@@ -21,8 +45,16 @@ function up()
 }
 function down_helper()
 {
-    var a = down();
-    console.log(a);
+    if(down_1 == -1)
+    {
+        var a = down();
+        console.log(a);
+        down_store = a;
+        down_1 = 0;
+        return;
+    }
+    console.log(down_store);
+
 }
 function down()
 {
@@ -31,26 +63,47 @@ function down()
 }
 function max()
 {
-    var temp = v[0];
+    if(max_1 == -1)
+    {
+        var temp = v[0];
     for(var i=0;i<v.length;i++)
     {
         temp = Math.max(v[i],temp);
     }
     console.log(temp);
+    max_store = temp;
+    max_1 = 0;
+    return;
+    }
+    console.log(max_store);
 }
 function min()
 {
+    if(min_1 == -1)
+    {
     var temp = v[0];
     for(var i=0;i<v.length;i++)
     {
         temp = Math.min(v[i],temp);
     }
     console.log(temp);
+    min_store = temp;
+    min_1= 0;
+    return;
+    }
+    console.log(min_store);
 }
 function sum_helper()
 {
-    var a = sum();
-    console.log(a);
+    if(sum_1 == -1)
+    {
+        var a = sum();
+        sum_store = a;
+        sum_1 = 0;
+        console.log(a);
+        return;
+    }
+    console.log(sum_store);
 }
 function sum()
 {
@@ -63,9 +116,18 @@ function sum()
 }
 function median()
 {
-    if(v.length%2 != 0)
-        console.log(v[Math.floor(v.length/2)]);
-    console.log((v[Math.floor(v.length/2)] + v[Math.floor((v.length-1)/2)])/2);
+    if(med_1 == -1)
+    {
+        var m =0;
+        if(v.length%2 != 0)
+            m = v[Math.floor(v.length/2)];
+        else
+            m = (v[Math.floor(v.length/2)] + v[Math.floor((v.length-1)/2)])/2;
+        med_1 = 0;
+        med_store =m;
+        return; 
+    }
+    console.log(med_store);
 }
 function mean()
 {
@@ -73,17 +135,33 @@ function mean()
 }
 function mean_helper()
 {
-    var a = mean();
-    console.log(a);
+    if(mean_1 == -1)
+    {
+        var a = mean();
+        console.log(a);
+        mean_store = a;
+        mean_1 = 0;
+        return;
+    }
+    console.log(mean_store);
 }
 function stdev()
 {
-    var temp = 0;
-    var t = mean();
-    for(var i=0;i<v.length;i++)
+    if(st_1 == -1)
     {
-        temp = temp + (v[i]-t)*(v[i]-t);
+        var temp = 0;
+        var t = mean();
+        for(var i=0;i<v.length;i++)
+        {
+            temp = temp + (v[i]-t)*(v[i]-t);
+        }
+        var a =  Math.sqrt(temp/v.length);
+        console.log(a);
+        st_1 = 0;
+        st_store = a;
+        return;
     }
-    console.log( Math.sqrt(temp/v.length));
+    console.log(st_store);
 }
+
 
